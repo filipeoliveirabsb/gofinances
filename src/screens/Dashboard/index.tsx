cf10e17ed509f7204e7e0c5a4704250cec430a0b
F3288636 Filipe Oliveira da Silva <filipe_oliveira@bb.com.br>
2022-02-02 13:16:32 -0300

    cards prontos

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,30 +1,63 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+
 
 import {
     Container,
-    Header, 
-    Photo,
+    Header,
+    UserWraper, 
+    UserInfo,
+    Icon, 
     User,
-    UserGreeting, 
-    UserInfo, 
-    UserName
+    Photo,
+    UserGreeting,
+    UserName,
+    HighlightCards
 } from './styles';
 
 export function Dashboard(){
     return (
         <Container>
             <Header>
-                <UserInfo>
-                    <Photo 
-                        source={{uri:'https://avatars.githubusercontent.com/u/65576264?v=4'}}
-                        
-                    />
-                    <User>
-                        <UserGreeting>Olá, </UserGreeting>
-                        <UserName>Fulano</UserName>
-                    </User>
-                </UserInfo>
+
+                <UserWraper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ 
+                                uri: "https://avatars.githubusercontent.com/u/65576264?v=4",
+                                
+                            }}
+                            />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Fulano</UserName>
+                        </User>
+                    </UserInfo>
+                    
+                    <Icon name='power'/>        
+                </UserWraper>
+
             </Header>
+            <HighlightCards>
+                <HighlightCard 
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 à 16 de abril"
+                />
+            </HighlightCards>
         </Container>
     );
 }
